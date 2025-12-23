e8d1f283f03f5ff59b9961ec426e78779d3a7d7d
pavelievilya <dvlmzt52@gmail.com>
2025-12-23 09:46:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Исходный список
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+Console.WriteLine("Полный список: " + string.Join(", ", numbers));
+int n = 3;
+var firstElements = numbers.Take(n).ToList();
+
+Console.WriteLine($"Первые {n} элемента: " + string.Join(", ", firstElements));
